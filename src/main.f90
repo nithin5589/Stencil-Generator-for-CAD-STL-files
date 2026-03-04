3bfdfc076663c13ace88103f5a4bfcda80003625
Nithin V K <am.en.u4me16143@tamu.edu>
2026-03-03 22:59:52 -0600

    Add files via upload

--- src/main.f90
+++ src/main.f90
@@ -3,12 +3,11 @@ program MainProgram
     character(len=100) :: command
 
     ! -----------------------------------------------------------------------------------------------
-    ! Delete Folders containing the previous data points and Recreating them
+    ! Delete Folders containing the previous data points and Recreate them
     ! -----------------------------------------------------------------------------------------------
     ! Delete the folder named 'stlProc'
     command = "rm -rf stlProc"
     call system(command)
-
     ! Create the folder named 'stlProc'
     command = "mkdir stlProc"
     call system(command)
@@ -16,97 +15,67 @@ program MainProgram
     ! Delete the folder named 'slicePlanes'
     command = "rm -rf slicePlanes"
     call system(command)
-
     ! Create the folder named 'slicePlanes'
     command = "mkdir slicePlanes"
-    call system(command)
-
-    ! Delete the folder named 'lineIntersections'
-    command = "rm -rf lineIntersections"
-    call system(command)
-
-    ! Create the folder named 'lineIntersections'
-    command = "mkdir lineIntersections"
     call system(command)
 
     ! Delete the folder named 'sorted'
     command = "rm -rf sorted"
     call system(command)
-
     ! Create the folder named 'sorted'
     command = "mkdir sorted"
     call system(command)
 
-    ! Delete the folder named '3D_Initial_Grid'
-    command = "rm -rf 3D_Initial_Grid"
+    ! Delete the folder named 'lineIntersections'
+    command = "rm -rf lineIntersections"
+    call system(command)
+    ! Create the folder named 'lineIntersections'
+    command = "mkdir lineIntersections"
     call system(command)
 
-    ! Create the folder named '3D_Initial_Grid'
-    command = "mkdir 3D_Initial_Grid"
+    ! Delete the folder named '2D_All_Points'
+    command = "rm -rf 2D_All_Points"
     call system(command)
-
-    ! Delete the folder named '2D_All_Inside_Points'
-    command = "rm -rf 2D_All_Inside_Points"
-    call system(command)
-
-    ! Create the folder named '2D_All_Inside_Points'
-    command = "mkdir 2D_All_Inside_Points"
+    ! Create the folder named '2D_All_Points'
+    command = "mkdir 2D_All_Points"
     call system(command)
 
     ! Delete the folder named '2D_All_Contour_Points'
     command = "rm -rf 2D_All_Contour_Points"
     call system(command)
-
     ! Create the folder named '2D_All_Contour_Points'
     command = "mkdir 2D_All_Contour_Points"
     call system(command)
 
-    ! Delete the folder named '2D_All_Contour_Points_Final'
-    command = "rm -rf 2D_All_Contour_Points_Final"
+    ! Delete the folder named '2D_IP_BP_Data'
+    command = "rm -rf 2D_IP_BP_Data"
     call system(command)
-
-    ! Create the folder named '2D_All_Contour_Points_Final'
-    command = "mkdir 2D_All_Contour_Points_Final"
-    call system(command)
-
-    ! Delete the folder named '2D_Interior_Points_Data'
-    command = "rm -rf 2D_Interior_Points_Data"
-    call system(command)
-
-    ! Create the folder named '2D_Interior_Points_Data'
-    command = "mkdir 2D_Interior_Points_Data"
-    call system(command)
-
-    ! Delete the folder named '2D_Boundary_Points_Data'
-    command = "rm -rf 2D_Boundary_Points_Data"
-    call system(command)
-
-    ! Create the folder named '2D_Boundary_Points_Data'
-    command = "mkdir 2D_Boundary_Points_Data"
+    ! Create the folder named '2D_IP_BP_Data'
+    command = "mkdir 2D_IP_BP_Data"
     call system(command)
 
     ! Delete the folder named '3D_All_Points'
     command = "rm -rf 3D_All_Points"
     call system(command)
-
     ! Create the folder named '3D_All_Points'
     command = "mkdir 3D_All_Points"
     call system(command)
+
     ! -----------------------------------------------------------------------------------------------
     
     ! -----------------------------------------------------------------------------------------------
-    ! Commands to run the other Fortran file
+    ! Commands to run the codes in-order
     ! -----------------------------------------------------------------------------------------------
     command = "gfortran stlRead.f90 -o stlRead"
     call system(command)
 
-    command = "./stlRead"
+    command = "./stlRead Rectangle.STL"
     call system(command)
 
     command = "gfortran slicing.f90 -o slicing"
     call system(command)
 
-    command = "./slicing"
+    command = "./slicing 1.000" ! Declare dz
     call system(command)
 
     command = "gfortran parametricLineEquationCalculator.f90  -o  parametricLineEquationCalculator"
@@ -115,35 +84,35 @@ program MainProgram
     command = "./parametricLineEquationCalculator"
     call system(command)
 
-    command = "gfortran sort2.f90 -o sort2"
+    command = "gfortran sorting.f90 -o sorting"
     call system(command)
 
-    command = "./sort2"
+    command = "./sorting"
     call system(command)
 
-    ! command = "gfortran fdm_mesh_generator.f90 -o fdm_mesh_generator"
-    ! call system(command)
+    command = "gfortran gridGenerator.f90 -o gridGenerator"
+    call system(command)
 
-    ! command = "./fdm_mesh_generator"
-    ! call system(command)
+    command = "./gridGenerator 1.000 1.000" ! Declare dx, dy
+    call system(command)
 
-    ! command = "gfortran Final_Interior_and_Boundary_Points.f90 -o Final_Interior_and_Boundary_Points"
-    ! call system(command)
+    command = "gfortran multiContourCheck2D.f90 -o multiContourCheck2D"
+    call system(command)
 
-    ! command = "./Final_Interior_and_Boundary_Points"
-    ! call system(command)
+    command = "./multiContourCheck2D"
+    call system(command)
 
-    ! command = "gfortran Interior_and_Boundary_2D_V24.f90 -o Interior_and_Boundary_2D_V24"
-    ! call system(command)
+    command = "gfortran IPandBP2D.f90 -o IPandBP2D"
+    call system(command)
 
-    ! command = "./Interior_and_Boundary_2D_V24"
-    ! call system(command)
+    command = "./IPandBP2D 1.000 1.000 1.000" ! Declare dx, dy, dz
+    call system(command)
 
-    ! command = "gfortran Grid_3D_Generator.f90 -o Grid_3D_Generator"
-    ! call system(command)
+    command = "gfortran stencilGenerator3D.f90 -o stencilGenerator3D"
+    call system(command)
 
-    ! command = "./Grid_3D_Generator"
-    ! call system(command)
+    command = "./stencilGenerator3D 1.000 1.000 1.000" ! Declare dx, dy, dz
+    call system(command) 
     ! -----------------------------------------------------------------------------------------------
     
 end program MainProgram
